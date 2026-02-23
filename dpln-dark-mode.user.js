ad9b5180332c5cfb31560341724d9267e5a419e8
BurN-30 <saccol.nathan@yahoo.fr>
2026-02-23 03:26:47 +0100

    v14.9 - Increase main-wrap margin-top to 60px

--- dpln-dark-mode.user.js
+++ dpln-dark-mode.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Dofus pour les Noobs - Dark Mode
 // @namespace    http://tampermonkey.net/
-// @version      14.8
+// @version      14.9
 // @description  Thème sombre complet pour DPLN avec bouton toggle.
 // @author       BurN-30
 // @match        *://www.dofuspourlesnoobs.com/*
@@ -45,7 +45,7 @@
             display: none !important;
         }
         body.dpln-dark #main-wrap {
-            margin-top: 20px !important;
+            margin-top: 60px !important;
         }
         body.dpln-dark #nav-wrap,
         body.dpln-dark #wsite-page-wrapper,
